8a6c39505fb752a504b6556fa8fa05ba270871c3
LA FREE <76892770+lucyjilly@users.noreply.github.com>
2021-01-10 18:31:49 +0000

    Update SectionA Q1.py

--- SectionA Q1.py
+++ SectionA Q1.py
@@ -5,23 +5,32 @@ Created on Tue Jan  5 05:53:47 2021
 @author: lafree
 """
           
- #Count all unique four letter words in the file. 
-words = set()
+ # -*- coding: utf-8 -*-
+"""
 
+"""
+
+
+#Question 1:Counting all unique four letter words in the file. 
+    #creating an empty set 
+words = set()
+    #using with statement    
 with open ("C:\\Users\\LUCY\\Downloads\\words (1).txt", 'r') as word_stimuli:
-    for line in word_stimuli:
+    for line in word_stimuli:        
+     #using a dictionary to define key areas to be adhered to:         
        unique_4Lwords = {word for word in line.lower().split() if len(word)==4 and "'" not in word}
        words.update(unique_4Lwords)
-    unique_word_stimuli = (len(words))
+    unique_word_stimuli = (len(words)) 
+    #printing out the number of my unique four letter words.  
     print("Uniquefour: " + str(unique_word_stimuli))
    
-    assert unique_word_stimuli == 2981
-        
+#Question 2:Dividing the four letter words based upon the last two letters of the word    
+    #creating a list  
 word_list = []
-for word in words:
+    #Using for statement  
+for word in words:  
+    #Checking the last two characters and frequency   
     word_list.append(word[-2:])
-    
-
 def character_frequency(word_list):
     word_set = set(word_list)
 
@@ -33,8 +42,19 @@ def character_frequency(word_list):
         
     return word_frequency
 
-        
-      
+#Question 3: Printing the list of word endings and their number
+ 
+print(character_frequency(word_list))
 
+#Question 4:Selecting one word to create a stimulus list
+    #Initializing 'stimulus list' 
+stimulus_list = []
 
+for x in words:
+    for word in x.split():
+        if word.endswith('ps'):
+    #Question 4: Print list of chosen word stimuli        
+            print(word)
 
+#Closing file
+word_stimuli.close()
